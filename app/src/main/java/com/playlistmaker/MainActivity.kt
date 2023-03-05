2848dc65a894aa1974a6e93c8b4d445064f9ea1a
OlegQt <xqjh@mail.ru>
2023-03-05 11:00:40 +0300

    Activity search and intent added

--- app/src/main/java/com/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/playlistmaker/MainActivity.kt
@@ -1,47 +1,29 @@
 package com.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    var btnSettings:Button?=null
+    var btnSettings: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setContentView(R.layout.activity_settings)
 
         val btnMedia = findViewById<Button>(R.id.media)
         val btnSearch = findViewById<Button>(R.id.search)
-        btnSettings = findViewById(R.id.settings) // Попробовал объявить переменную кнопки в поле класса без later-int
+        // Попробовал объявить переменную кнопки в поле класса без later-int
+        btnSettings = findViewById(R.id.settings)
+
+        // Объявление интентов
+        val displayIntent = Intent(this.baseContext, ActivitySettings::class.java)
 
 
-        val searchBtnListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Поиск (через анонимный класс)",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-
-        btnSearch.setOnClickListener(searchBtnListener)
-        btnSettings?.setOnClickListener{
-            val toast = Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT)
-            toast.show()
-        }
-        btnMedia.setOnClickListener { v ->
-            val toast =
-                Toast.makeText(this@MainActivity, "Media ${v.width.toString()}", Toast.LENGTH_SHORT)
-            toast.show()
-        }
-
-
-
+        btnSearch.setOnClickListener { }
+        btnMedia.setOnClickListener { }
+        btnSettings?.setOnClickListener { startActivity(displayIntent) }
 
     }
 }
