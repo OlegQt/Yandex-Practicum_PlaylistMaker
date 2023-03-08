1ff9d93febe47fd9085aced1b61c31a0641927d1
OlegQt <xqjh@mail.ru>
2023-03-08 10:31:28 +0300

    Little change

--- app/src/main/java/com/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/playlistmaker/MainActivity.kt
@@ -18,9 +18,13 @@ class MainActivity : AppCompatActivity() {
         btnSettings = findViewById(R.id.settings)
 
         // Объявление интентов
-        val settingsScreen = Intent(this.baseContext, ActivitySettings::class.java)
-        val mediaScreen = Intent(this.baseContext, ActivityMedia::class.java)
-        val searchScreen = Intent(this.baseContext, ActivitySearch::class.java)
+        val settingsScreen = Intent(this, ActivitySettings::class.java)
+        val mediaScreen = Intent(this, ActivityMedia::class.java)
+        val searchScreen = Intent(this, ActivitySearch::class.java)
+
+        //btnSearch.setOnClickListener { finish() }
+        // Не понял ваш комментарий по поводу исопльзования finish()
+        // он завершает тогда текущую активити и все. Объясните плз кодом, что вы имеете в виду?
 
         btnSearch.setOnClickListener { startActivity(searchScreen) }
         btnMedia.setOnClickListener { startActivity(mediaScreen) }
